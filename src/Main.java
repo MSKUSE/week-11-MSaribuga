a626b00e0692cff9d304f32b5a2a5e840face198
MSaribuga <mustafasaribuga2018@gmail.com>
2025-05-22 12:54:54 +0300

    week11

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Stack stack = new StackLinkedList();
-        stack.push("This is a string");
-        stack.push(5);
-        stack.peek();
-        System.out.println("Is empty "
-        +stack.isEmpty() );
-        System.out.println("Pop = " +
-                stack.pop());
-        stack.peek();
+        try {
+            stack.pop();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+/*
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        stringArrayList.add("123");
+        HashMap<String, Double> grades = new HashMap<>();
+        grades.put("Mustafa", 100.0);
+        System.out.println(grades);
+        grades.put("Pelin", 0.0);
+        System.out.println(grades);
+        grades.put("Emre", 99.0);
+        System.out.println(grades);
+        grades.put("Pelin", 30.0);
+        System.out.println(grades);
+        grades.put("yasin", 100.0);
+        System.out.println(grades);
+        System.out.println(grades.keySet() instanceof Set);
+*/
+
     }
 }
